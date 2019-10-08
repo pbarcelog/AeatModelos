7d77bab8acb6e58a8a2e729da2cb50d3b4e6c472
Manuel Diago Garcia <manuel@manuelyelena.com>
2019-10-08 08:49:21 +0200

    Sube versión 1.0.1.4
    Sube versión 1.0.1.4

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Irene Solutions SL")]
 [assembly: AssemblyProduct("AeatModelos")]
-[assembly: AssemblyCopyright("Copyright ©  Irene Solutions SL 2018")]
+[assembly: AssemblyCopyright("Copyright ©  Irene Solutions SL 2019")]
 [assembly: AssemblyTrademark("Irene Solutions SL")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los números de compilación y de revisión predeterminados
 // mediante el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.3")]
-[assembly: AssemblyFileVersion("1.0.1.3")]
+[assembly: AssemblyVersion("1.0.1.4")]
+[assembly: AssemblyFileVersion("1.0.1.4")]
